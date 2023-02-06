4fc7449cfaea93e4aedfcc42db34e862ccefeaa3
StevenY21 <stevenyao2016@gmail.com>
2023-02-06 02:47:25 -0500

    passed testing for all 3 questions

--- assigns/02/MySolution/assign02_02.py
+++ assigns/02/MySolution/assign02_02.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append('..')
 from assign02 import *
+from assign02_01 import *
 ####################################################
 print("[import ./../assign02.py] is done!")
 ####################################################
@@ -10,3 +11,26 @@ print("[import ./../assign02.py] is done!")
 # mylist_quicksort (see list_quicksort in assign02.sml)
 #
 ####################################################
+def mylist_quicksort(xs):
+    def qsort(xs):
+        if mylist_nilq(xs) == True:
+            return mylist_nil()
+        else:
+            yszs = qpart(xs.cons2, xs.cons1)
+            ys = qsort(yszs[0])
+            x1 = mylist_cons(xs.cons1, mylist_nil())
+            zs = qsort(yszs[1])
+            qsortlist = mylist_append(x1,zs)
+            qsortlist = mylist_append(ys, qsortlist)
+            return qsortlist
+        
+    def qpart(xs, p0):
+        if mylist_nilq(xs) == True:
+            return (mylist_nil(), mylist_nil())
+        else:
+            yszs = qpart(xs.cons2, p0)
+            if xs.cons1 <= p0:
+                return (mylist_cons(xs.cons1, yszs[0]), yszs[1])
+            else:
+                return (yszs[0], mylist_cons(xs.cons1, yszs[1]))
+    return qsort(xs)
--- assigns/02/MySolution/assign02_03.py
+++ assigns/02/MySolution/assign02_03.py
@@ -10,3 +10,32 @@ print("[import ./../assign02.py] is done!")
 # mylist_mergesort (see list_mergesort in assign02.sml)
 #
 ####################################################
+def mylist_mergesort(xs):
+    def split(xs):
+        if mylist_nilq(xs) == True:
+            return (mylist_nil(), mylist_nil())
+        else:
+            if mylist_nilq(xs.cons2) == True:
+                return (mylist_cons(xs.cons1, mylist_nil()), mylist_nil())
+            else:
+                yszs = split(xs.cons2.cons2)
+                return (mylist_cons(xs.cons1, yszs[0]), mylist_cons(xs.cons2.cons1, yszs[1]))
+    def merge(ys, zs):
+        if mylist_nilq(ys) == True:
+            return zs
+        else:
+            if mylist_nilq(zs) == True:
+                return mylist_cons(ys.cons1, ys.cons2)
+            else:
+                if ys.cons1 <= zs.cons1:
+                    return mylist_cons(ys.cons1, merge(ys.cons2, mylist_cons(zs.cons1, zs.cons2)))
+                else:
+                    return mylist_cons(zs.cons1, merge(mylist_cons(ys.cons1, ys.cons2), zs.cons2))
+    if mylist_nilq(xs) == True:
+        return mylist_nil()
+    else:
+        if mylist_nilq(xs.cons2) == True:
+            return mylist_cons(xs.cons1, mylist_nil())
+        else:
+            yszs = split(xs.cons2.cons2)
+            return merge(mylist_mergesort(mylist_cons(xs.cons1, yszs[0])), mylist_mergesort(mylist_cons(xs.cons2.cons1, yszs[1])))
