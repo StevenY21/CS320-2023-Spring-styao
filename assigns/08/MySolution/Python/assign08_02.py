56503b9a3e1dfd6bf8c4b3eed92c70e8952983ee
StevenY21 <stevenyao2016@gmail.com>
2023-04-13 20:44:50 -0400

    finished 08

--- assigns/08/MySolution/Python/assign08_02.py
+++ assigns/08/MySolution/Python/assign08_02.py
@@ -16,5 +16,48 @@ def generator_merge2(gen1, gen2, lte3):
     The function generator_merge2 is expected to work correctly
     for both finite and infinite generators.
     """
-    raise NotImplementedError
+    gen1x = next(gen1, None)
+    gen2x = next(gen2, None)
+    temp = None
+    # initial statements to get a temp value
+    if lte3(gen1x, gen2x): #if gen1 provides smaller value
+        temp = (gen2x,2) # hold onto gen2 val
+        yield gen1x
+    else: # if gen2 gets smaller value
+        temp = (gen1x,1) #hold onto gen1 val
+        yield gen2x
+    while True:
+        if temp[0] == None: # when one of the generators are done
+            if temp[1] == 1: # if gen1 done
+                gen2x = next(gen2, None)
+                if gen2x == None:
+                    return None
+                else:
+                    yield gen2x
+            else: # if gen2 done
+                gen1x = next(gen1, None)
+                if gen1x == None:
+                    return None
+                else:
+                    yield gen1x
+        elif temp[1] == 1: # if the holding value from gen1
+            gen2x = next(gen2, None)
+            if gen2x == None: # if nothing in gen2 anymore
+                yield temp[0]
+                temp = (gen2x, 2)
+            elif lte3(temp[0], gen2x): # if gen1 value smaller
+                yield temp[0]
+                temp = (gen2x,2)
+            else: # if gen2 value still smaller
+                yield(gen2x)
+        else: # if holding gen2 value
+            gen1x = next(gen1, None)
+            if gen1x == None: #nothing more in gen1
+                yield temp[0]
+                temp = (gen1x, 1)
+            elif lte3(gen1x, temp[0]):
+                yield gen1x
+            else:
+                yield temp[0]
+                temp = (gen1x, 1)
 ####################################################
--- assigns/08/MySolution/Python/assign08_03.py
+++ assigns/08/MySolution/Python/assign08_03.py
@@ -1,6 +1,7 @@
 ####################################################
 #!/usr/bin/env python3
 ####################################################
+import queue
 """
 HX-2023-04-07: 20 points
 The following implementation is stream-based:
@@ -21,13 +22,24 @@ The following implementation is stream-based:
 #     qnxs = queue.Queue()
 #     for nx0 in nxs:
 #         qnxs.put(nx0)
-#         visited.add(nx1)
+#         visited.add(nx0)
 #     return lambda: helper(qnxs)
 Please give a generator-based implementation of graph_bfs!!!
 """
+
+
 def generator_graph_bfs(nxs, fnexts):
-    """
-    This function does the same as graph_bfs.
-    """
-    raise NotImplementedError
+    visited = set()
+    qnxs = queue.Queue()
+    qnxs.put(nxs[0])
+    visited.add(nxs[0])
+    while not (qnxs.empty()):
+        nx1 = qnxs.get()
+        # print("gtree_bfs: helper: nx1 = ", nx1)
+        for nx2 in fnexts(nx1):
+            if (not nx2 in visited):
+                qnxs.put(nx2)
+                visited.add(nx2)
+        yield nx1
+
 ####################################################
